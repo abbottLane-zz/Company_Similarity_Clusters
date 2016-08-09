e28fb2b4f81718fea26a1e78cdd61c9f9a40e3a3
wlane <will@asklane.com>
2016-08-09 00:05:47 -0700

    Recommendations now sorted by cosine spatial distance (least distance first)

--- CompanyRecommender.py
+++ CompanyRecommender.py
@@ -1,6 +1,6 @@
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
-from Utilities import output_results, load_data
+from Utilities import output_results, load_data, get_sorted_companies
 
 
 class CompanyRecommender:
@@ -9,19 +9,23 @@ class CompanyRecommender:
         self.k = k
         self.results = {}
         self.company_to_cluster = {}
+        self.company_idx_map = {}
         self.companies = []
         self.descriptions = []
+        self.instance_vector_array = None
         self.km = None
         self.vectorizer = None
         self.__initialize_clusters()
 
     def __initialize_clusters(self):
         # Load the data
-        self.companies, self.descriptions = load_data(self.data_dir, has_header=True)
+        self.companies, self.descriptions, self.company_idx_map = load_data(self.data_dir, has_header=True)
 
         # Vectorize the data using TF-IDF to help reduce dimensionality
         self.vectorizer = TfidfVectorizer(max_df=0.5, min_df=2, stop_words='english', use_idf=True, ngram_range=(1, 2))
         X = self.vectorizer.fit_transform(self.descriptions)
+        self.instance_vector_array = X.toarray()
+
         print("n_samples: %d, n_features: %d" % X.shape)
 
         # Initialize K-means algorithm preferences
@@ -49,12 +53,20 @@ class CompanyRecommender:
         if company_name in self.company_to_cluster:
             cluster_no = self.company_to_cluster[company_name]
             cluster = self.results[cluster_no]
+
+            # Sort the company's cluster by cosine spatial distance
+            sorted_companies = get_sorted_companies(cluster, company_name, self.company_idx_map, self.instance_vector_array)
+
             # Append all the companies similar to the given company to a string and return
             similar_str ="["
-            for company in cluster:
+            for company_dist_tuple in sorted_companies:
+                company = company_dist_tuple[0]
+                distance_from_centroid_company = company_dist_tuple[1]
                 if company_name != company.name:
-                    similar_str += company.name + ", "
-            similar_str = similar_str[:len(similar_str)-2] + "]"
+                    similar_str += company.name + " : " + str(distance_from_centroid_company) + ", "
+            similar_str = similar_str[:len(similar_str)-2] + "]" # similar_str = similar_str minus the last character, which is a comma
         else:
-            similar_str = "Sorry, that company name does not appear in the given data. "
+            similar_str = "Sorry, the company name \"" + company_name + "\" does not appear in the given data. "
         return similar_str
+
+
--- Utilities.py
+++ Utilities.py
@@ -1,5 +1,8 @@
 import csv
+import operator
+
 from CompanyDoc import CompanyDoc
+from scipy import spatial
 
 
 def load_data(file_path, has_header):
@@ -7,14 +10,18 @@ def load_data(file_path, has_header):
         reader = csv.reader(csvfile, delimiter=',')
         companies = []
         descriptions = []
+        company_idx_map = {}
         row_count = 0
+        company_idx = 0
         for row in reader:
-            if has_header and row_count != 0:
+            if has_header and row_count != 0:  # Skip the header
                 company = CompanyDoc(row[0], row[1], row[2].split("\""))
                 companies.append(company)
                 descriptions.append(row[1])
+                company_idx_map[row[0]] = company_idx
+                company_idx += 1
             row_count += 1
-    return companies, descriptions
+    return companies, descriptions, company_idx_map
 
 
 def output_results(km, k, vectorizer, results):
@@ -30,12 +37,36 @@ def output_results(km, k, vectorizer, results):
 
 
 def write_top_terms_in_cluster(k, km, vectorizer):
-        order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-        terms = vectorizer.get_feature_names()
-        with open("Output/_Cluster_Top_Terms.txt", "w") as file:
-            for i in range(k):
-                file.write("Cluster %d:" % i + "\n")
-                file.write("\tTop 10 Dimensions: ")
-                for ind in order_centroids[i, :10]:
-                    file.write(' %s' % terms[ind] + ", ")
-                file.write("\n")
+    order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+    terms = vectorizer.get_feature_names()
+    with open("Output/_Cluster_Top_Terms.txt", "w") as file:
+        for i in range(k):
+            file.write("Cluster %d:" % i + "\n")
+            file.write("\tTop 10 Dimensions: ")
+            for ind in order_centroids[i, :10]:
+                file.write(' %s' % terms[ind] + ", ")
+            file.write("\n")
+
+
+def get_cluster_of_vectors(cluster, vector_array, company_idx_map):
+    vectors = list()
+    for company in cluster:
+        vectors.append((vector_array[company_idx_map[company.name]], company))
+    return vectors
+
+
+def sort_company_vectors(cluster_company_vectors, centroid_vector):
+    company_vector_tuples = list()
+    for vec in cluster_company_vectors:
+        distance = spatial.distance.cosine(vec[0], centroid_vector)
+        company_vector_tuples.append((vec[1], distance))
+    company_vector_tuples.sort(key=operator.itemgetter(1))
+    return company_vector_tuples
+
+
+def get_sorted_companies(cluster, company_name, company_idx_map, vector_array):
+    centroid_company_idx = company_idx_map[company_name]
+    centroid_vector = vector_array[centroid_company_idx]
+    cluster_company_vectors = get_cluster_of_vectors(cluster, vector_array, company_idx_map)
+    sorted_company_vectors = sort_company_vectors(cluster_company_vectors, centroid_vector)
+    return sorted_company_vectors
